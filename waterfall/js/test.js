48a01db0fc067a6589d9f4f77bd7cb76cafa5e13
freshmilk10 <707199408@qq.com>
2019-04-17 13:33:50 +0800

    1.0.1

--- waterfall/js/test.js
+++ waterfall/js/test.js
@@ -1,16 +1,16 @@
 var json = { 
     data:[ 
-        {name:'花瓣小小裙花瓣',price:'128',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_01.jpg'}, 
-        {name:'时尚牛仔短裤',price:'298',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_02.jpg'}, 
-        {name:'白色婚纱',price:'668',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_03.jpg'}, 
-        {name:'绿色防晒衣',price:'218',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_04.jpg'}, 
-        {name:'格纹小短裙',price:'88',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_05.jpg'}, 
-        {name:'复古旗袍',price:'128',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_06.jpg'}, 
-        {name:'花瓣小小裙花瓣',price:'128',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_07.jpg'}, 
-        {name:'时尚牛仔短裤',price:'298',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_08.jpg'}, 
-        {name:'白色婚纱',price:'668',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_09.jpg'}, 
-        {name:'绿色防晒衣',price:'218',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_10.jpg'}, 
-        {name:'格纹小短裙',price:'118',details:'质量超好特别的舒适，夏天首选。',src:'images/temporary/img_11.jpg'} 
+        {name:'花瓣小小裙花瓣',price:'128',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_01.jpg'}, 
+        {name:'时尚牛仔短裤',price:'298',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_02.jpg'}, 
+        {name:'白色婚纱',price:'668',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_03.jpg'}, 
+        {name:'绿色防晒衣',price:'218',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_04.jpg'}, 
+        {name:'格纹小短裙',price:'88',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_05.jpg'}, 
+        {name:'复古旗袍',price:'128',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_06.jpg'}, 
+        {name:'花瓣小小裙花瓣',price:'128',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_07.jpg'}, 
+        {name:'时尚牛仔短裤',price:'298',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_08.jpg'}, 
+        {name:'白色婚纱',price:'668',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_09.jpg'}, 
+        {name:'绿色防晒衣',price:'218',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_10.jpg'}, 
+        {name:'格纹小短裙',price:'118',details:'质量超好特别的舒适，夏天首选。',src:'../images/temporary/img_11.jpg'} 
     ] 
 } 
 
